8d8a36591b2d8cf03e4237ae0acd9d2356bf2585
tygrxqt <hi@tygr.dev>
2024-07-19 00:04:34 +0100

    fix: missing key prop in album map

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -14,29 +14,24 @@ export default async function MusicPage() {
 				</div>
 				<div className="flex flex-col gap-4 items-center pb-4 w-full">
 					{albumData ? (
-						<>
-							<div className="grid gap-4 grid-cols-2 sm:grid-cols-3">
-								{albumData?.topalbums.album.slice(0, 48).map((album) => {
-									return (
-										<>
-											<AlbumCard
-												key={album.name}
-												artist={album.artist.name}
-												name={truncate(album.name, 25)}
-												coverImage={album.image[3]["#text"]}
-												href={album.url}
-											/>
-										</>
-									);
-								})}
-							</div>
-						</>
+						<div className="grid gap-4 grid-cols-2 sm:grid-cols-3">
+							{albumData?.topalbums.album.slice(0, 48).map((album, i) => {
+								return (
+									<div key={i}>
+										<AlbumCard
+											artist={album.artist.name}
+											name={truncate(album.name, 25)}
+											coverImage={album.image[3]["#text"]}
+											href={album.url}
+										/>
+									</div>
+								);
+							})}
+						</div>
 					) : (
-						<>
-							<p className="text-neutral-500 dark:text-neutral-400">
-								Failed to fetch top albums.
-							</p>
-						</>
+						<p className="text-neutral-500 dark:text-neutral-400">
+							Failed to fetch top albums.
+						</p>
 					)}
 				</div>
 			</div>
