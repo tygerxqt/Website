4f7e3d75e74da63283d42e5be8b5da0f3c1b6551
Ty Mason <hi@tygr.dev>
2024-10-24 19:26:48 +0100

    feat: update now-playing style

--- components/now-playing/index.tsx
+++ components/now-playing/index.tsx
@@ -9,14 +9,23 @@ export default async function NowPlaying() {
 	return (
 		<div className="flex flex-row items-center gap-1 justify-center w-full py-4">
 			<Icons.Spotify className="w-6 h-6 mr-2" />
-			<span className="text-neutral-500">
-				{data.is_playing ? "Now Playing:" : "Spotify - Not Playing"}
-			</span>
-			{data.is_playing && (
-				<Link href={data.item!.external_urls.spotify} target="_blank">
+			{data.is_playing ? (
+				<>
+					<span className="text-neutral-500">
+						Listening to: {" "}
+					</span>
+
+					<Link href={data.item!.external_urls.spotify} target="_blank">
+						<Button variant="link" className="px-0">
+							{data.item!.name} -{" "}
+							{data.item!.artists.map((artist) => artist.name).join(", ")}
+						</Button>
+					</Link>
+				</>
+			) : (
+				<Link href="https://open.spotify.com/user/z4is4ny666qn1njee0k6g77o2?si=275c31679b264854" target="_blank">
 					<Button variant="link" className="px-0">
-						{data.item!.name} -{" "}
-						{data.item!.artists.map((artist) => artist.name).join(", ")}
+						Spotify - Nothing Playing
 					</Button>
 				</Link>
 			)}
